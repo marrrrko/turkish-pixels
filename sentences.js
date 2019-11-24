a1188bdc483aecdeafa240a2d794980c136a1f26
Mark Carrier <markcarrier@gmail.com>
2019-11-24 11:49:54 +0300

    Adds random sentence present continuous game to command line mode

--- sentences.js
+++ sentences.js
@@ -2,20 +2,31 @@ const vocabulary = require('./vocabulary')
 const _ = require('lodash')
 const turkish = require('./turkish')
 const readline = require('readline')
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-
-
+const delay = function(delay) {
+    return new Promise(function(resolve) {
+        setTimeout(() => resolve(), delay)
+    })
+}
 async function runAsCommandLineProgram() {
-    let words = await vocabulary.loadWordDatabase()
+    let wordDatabase = await vocabulary.loadWordDatabase()
 
+    while(true) {
+        await askARandomTurkishQuestion(wordDatabase)
+        await delay(4000)  
+    }
+    
+}
+
+async function askARandomTurkishQuestion(wordDatabase) {
     //let sentence = buildVerbSubjectObjectSentence(words, words.verbTenses)
-    let sentence = buildVerbSubjectSentence(words, words.verbTenses.filter(t => t.english == "present continuous"))
+    let sentence = buildVerbSubjectSentence(
+                    wordDatabase,
+                    wordDatabase.verbTenses.filter(t => t.english == "present continuous"),
+                    _.random(0,1),
+                    _.random(0,1))
 
-    console.log(`\nSentence to build:`)
+    console.log(`\n======================================\n`)
+    console.log(`Sentence to build:`)
     console.log(`  Verb: ${sentence.verb.english}`)
     if(sentence.object)
         console.log(`  Object: ${sentence.object.english}`)
@@ -25,11 +36,23 @@ async function runAsCommandLineProgram() {
         subjectHint = sentence.subject.isPlural ? " (plural)" : " (singular)"
     }
     console.log(`  Subject: ${sentence.subject.english}${subjectHint}`)
-    console.log(`  Tense: ${sentence.tense.english}`)
+    
+    let tenseHints = []
+    if(sentence.negativeForm)
+        tenseHints.push("negative")
+    if(sentence.questionForm)
+        tenseHints.push("question")
+    
+    let tenseHint = ""
+    if(tenseHints.length)
+        tenseHint = `, ${tenseHints.join(" ")}`
+    console.log(`  Tense: ${sentence.tense.english}${tenseHint}`)
+
     await new Promise(function(resolve, reject) {
-        rl.question('\nHit enter to view the answer ', (answer) => {      
-            console.log(`\n  ${sentence.translation}\n`)
-            rl.close();
+        rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+        rl.question('', (answer) => {      
+            console.log(`Answer: ${_.capitalize(sentence.translation)}`)          
+            rl.close()
             resolve()
         });
     })
@@ -46,21 +69,30 @@ function buildVerbSubjectObjectSentence(wordDatabase) {
     return sentence
 }
 
-function buildVerbSubjectSentence(wordDatabase, allowedTenses) {
+function buildVerbSubjectSentence(wordDatabase, allowedTenses, negativeFormAllowed, questionFormAllowed) {
     let sentence = {}
 
     if(allowedTenses == null)
         allowedTenses = wordDatabase.verbTenses
 
     sentence.verb = _.sample(wordDatabase.verbs)
-    sentence.tense = _.sample(allowedTenses)
     sentence.subject = _.sample(wordDatabase.pronouns)
+    sentence.tense = _.sample(allowedTenses)
+    sentence.negativeForm = false
+    if(negativeFormAllowed) {
+        sentence.negativeForm = _.sample([true, false, false])
+    }
+    if(questionFormAllowed) {
+        sentence.questionForm = _.sample([true, false, false])
+    }
 
     let conjugatedVerb = turkish.conjugateVerb(
         sentence.verb,
         sentence.tense,
         sentence.subject.person,
-        sentence.subject.isPlural)
+        sentence.subject.isPlural,
+        sentence.negativeForm,
+        sentence.questionForm)
 
     sentence.translation = `${sentence.subject.turkish} ${conjugatedVerb}`
 
